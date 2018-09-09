a17c59f87571d67aad16b8419c5cd10e046ae346
Shubham Burman <burman.shubham@tftus.com>
2018-09-09 15:08:25 +0530

    nav-bar styling

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -1,16 +1,55 @@
 import React from "react";
-import {Link} from "react-router";
+import {IndexLink, Link} from "react-router";
 
 
 export class Nav extends React.Component{
     render() {
         return (
-            <div>
-                Nav Component
-                <Link to="/">Get Weather</Link>
-                <Link to="/about">About</Link>
-                <Link to="/examples">Examples</Link>
+            <div className="top-bar">
+                <div className='top-bar-left'>
+                    <ul className='menu'>
+                        <li className="menu-text"> Weather App</li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className='top-bar-right'>
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search Weather"/>
+                            </li>
+                            <li>
+                                <input type="submit" className="button" value="Get Weather"/>
+                            </li>
+                        </ul>
+
+                    </form>
+                </div>
             </div>
+
         )
     }
+
+    onSearch(e) {
+        e.preventDefault();
+        alert('Not Wired up Yet!');
+    }
 }
+
+var old = (
+    <div>
+        Nav Component
+        <Link to="/">Get Weather</Link>
+        <Link to="/about">About</Link>
+        <Link to="/examples">Examples</Link>
+    </div>
+);
