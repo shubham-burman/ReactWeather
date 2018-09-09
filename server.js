879c5d8234fd09d948d498c1555adff0800a63f8
Shubham Burman <burman.shubham@tftus.com>
2018-09-09 13:10:10 +0530

    updated middleware for localhost

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(function(req, res, next) {
-    if(req.headers['x-forwarded-proto'] === 'http') {
-        next();
-    } else {
+    if(req.headers['x-forwarded-proto'] === 'https') {
         res.redirect('http://' + req.hostname + req.url);
+    } else {
+        next();
     }
 });
 app.use(express.static('public'));
