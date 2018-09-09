c1ac85f47557217ef5fb9d6251787d4793e56467
Shubham Burman <burman.shubham@tftus.com>
2018-09-09 10:10:49 +0530

    open weather https fix

--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const port = process.env.PORT || 3000;
 // create app
 
 var app = express();
+
+app.use(function(req, res, next) {
+    if(req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 app.use(express.static('public'));
 app.listen(port, function () {
     console.log("express server is up and running on port "+ port);
